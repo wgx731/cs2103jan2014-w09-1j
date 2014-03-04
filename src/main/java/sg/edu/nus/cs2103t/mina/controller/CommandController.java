b7f0bb49bd56145f907204c57899a544c73abed5
gndu <ud.connexus@gmail.com>
2014-03-04 20:24:13 +0800

    <feat>(TFM): Added 'completed' filter for TFM
    I had to use 'hg forget' to TDM because I made local changes to it else merging it will be a pain later

---
+++ src/main/java/sg/edu/nus/cs2103t/mina/controller/CommandController.java
@@ -0,0 +1,314 @@
+package sg.edu.nus.cs2103t.mina.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import sg.edu.cs2103t.mina.stub.DataParameterStub;
+import sg.edu.nus.cs2103t.mina.model.Task;
+import sg.edu.nus.cs2103t.mina.model.TaskType;
+import sg.edu.nus.cs2103t.mina.model.parameter.FilterParameter;
+import sg.edu.nus.cs2103t.mina.model.parameter.SearchParameter;
+import sg.edu.nus.cs2103t.mina.utils.DateUtil;
+
+public class CommandController {
+
+    private static String[] _inputString;
+	private static final int _maxInputStringArraySize = 2;
+	private static final int _modifyInputStringSplitSize = 3;
+	private static final int _userCommandPosition = 0;
+	private static final int _parameterPosition = 1;
+	private static final int _firstArrayIndex = 0;
+	private TaskDataManager TDM;
+	private TaskFilterManager TFM;
+	
+    enum CommandType {
+        ADD, DELETE, MODIFY, COMPLETE, DISPLAY, SEARCH, UNDO, EXIT, INVALID
+    };
+    
+    
+    // Constructor
+    public CommandController(){
+    	
+    }
+    
+    // This operation is used to get input from the user and execute it till
+    // exit
+    public void processUserInput(String userInput) {
+        CommandType command;
+        _inputString = userInput.split(" ", _maxInputStringArraySize);
+        TDM = new TaskDataManager();
+        TFM = new TaskFilterManager(TDM);
+        command = determineCommand();
+        processUserCommand(command);
+    }
+
+    // This operation is used to get the user input and extract the command from
+    // inputString
+    private CommandType determineCommand() {
+    	String userCommand = _inputString[_userCommandPosition];
+        CommandType command = CommandType.valueOf(userCommand.trim().toUpperCase());
+        return command;
+    }
+
+    // This operation is used to process the extracted command and call the
+    // respective functions
+    private void processUserCommand(CommandType command) {
+        switch (command) {
+            case ADD: {
+                DataParameterStub addParameter = processAddParameter(_inputString[_parameterPosition]);
+                // TaskDataManager.addTask(addParameter);
+                // ArrayList<ArrayList<String>> display =
+                // TaskFilterManager.getAllTask();
+                // call GUI display here
+                break;
+            }
+            case DELETE: {
+                // GUI: ask user confirmation
+                // isConfirmedDelete()
+                DataParameterStub deleteParameter = processMarkDeleteParameter(_inputString[_parameterPosition]);
+                // TaskDataManager.deleteTask(deleteparams);
+                // deleteparams: (int id)
+                // ArrayList<ArrayList<String>> display =
+                // TaskFilterManager.getAllTask();
+                // call GUI display here
+                break;
+            }
+            case MODIFY: {
+                DataParameterStub modifyParameter = processModifyParameter(_inputString[_parameterPosition]);
+                // TaskDataManager.editTask(addparams);
+                // editparams: (int id, String des) (int id, String des, Char
+                // prir) (int id, Char prir)
+                // (int id, String des, Date dead) (int id, Date dead)
+                // (int id, String des, Date start, Date end) (int id, Date
+                // start, Date end) // what happen if user want to change start
+                // or end only?
+                // ArrayList<ArrayList<String>> display =
+                // TaskFilterManager.getAllTask();
+                // call GUI display here
+                break;
+            }
+            case DISPLAY: {
+                String filterParameterString = _inputString[_parameterPosition];
+                if (!filterParameterString.isEmpty()){
+                	FilterParameter filterParam = processFilterParameter(filterParameterString);
+                	ArrayList<Task<?>> filterResult = TFM.filterTask(filterParam);
+                } else {
+                // to be continue..
+                }
+                // ArrayList<Task> display =
+                // TaskFilterManager.filterTask(filterparams);
+                // filterparams: (Filter object)
+                // call GUI display here
+                break;
+            }
+            case SEARCH: {
+                SearchParameter searchParameter = processSearchParameter(_inputString[_parameterPosition]);
+                ArrayList<Task<?>> searchResult = TFM.searchTasks(searchParameter);
+                // ArrayList<ArrayList<String>> display =
+                // TaskFilterManager.searchTask(searchparams);
+                // searchparams: (String word), (String word1, String word2,
+                // String word1 + word2), ...
+                // (algorithm: combination of words)
+                // call GUI display here
+                break;
+            } 
+            case COMPLETE:{
+            	DataParameterStub completeParameter = processMarkDeleteParameter(_inputString[_parameterPosition]);
+            	break;
+            }
+            case UNDO: {
+                // Hmm?
+                break;
+            }
+            case EXIT: {
+                System.exit(0);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+    
+    // This method process add parameter into a DataParameter instance
+    // @param parameterString
+    //			string contains parameter data
+    // @return addParam
+    //			DataParameter instance contains parameter for add method
+    
+    public DataParameterStub processAddParameter(String parameterString){
+    	DataParameterStub addParam = new DataParameterStub();
+    	ArrayList<String> parameters = new ArrayList<String>();
+    	for(String word : parameterString.split(" ")) {
+    	    parameters.add(word);
+    	}
+    	if (parameters.contains("-start")){
+    		addParam.setNewTaskType(TaskType.EVENT);
+    		int endIndexOfDescription = parameterString.indexOf("-");
+    		String description = parameterString.substring(0, endIndexOfDescription).trim();
+    		addParam.setDescription(description);
+    		int indexOfStartDate = parameters.indexOf("-start")+1;
+    		int indexOfEndDate = parameters.indexOf("-end")+1;
+    		try{
+    			Date startDate = DateUtil.parse(parameters.get(indexOfStartDate));
+    			Date endDate = DateUtil.parse(parameters.get(indexOfEndDate));
+    			addParam.setStartDate(startDate);
+    			addParam.setEndDate(endDate);
+    		} catch (Exception e){
+    			
+    		}    		
+    	} else if (parameters.contains("-end")){
+    		addParam.setNewTaskType(TaskType.DEADLINE);
+    		int endIndexOfDescription = parameterString.indexOf("-");
+    		String description = parameterString.substring(0, endIndexOfDescription).trim();
+    		addParam.setDescription(description);
+    		int indexOfEndDate = parameters.indexOf("-end")+1;
+    		try{
+    			Date endDate = DateUtil.parse(parameters.get(indexOfEndDate));
+    			addParam.setEndDate(endDate);
+    		} catch (Exception e){
+    			
+    		} 
+    	} else {
+    		addParam.setNewTaskType(TaskType.TODO);
+    		int endIndexOfDescription = parameterString.indexOf("-");
+    		String description;
+    		if (endIndexOfDescription!=-1){
+    			description = parameterString.substring(0, endIndexOfDescription).trim();
+    		} else {
+    			description = parameterString;
+    		}
+    		addParam.setDescription(description);
+    	}
+    	if (parameters.contains("-priority")){
+    		int indexOfPriority = parameters.indexOf("-priority")+1;
+    		char priority = parameters.get(indexOfPriority).toCharArray()[_firstArrayIndex];
+    		addParam.setPriority(priority);
+    	}
+    	return addParam;
+    }
+    
+    // This method process search parameter into a SearchParameter instance
+    // @param parameterString
+    //			string contains parameter data
+    // @return searchParam
+    //			SearchParameter instance contains parameter for search method
+    
+    public SearchParameter processSearchParameter(String parameterString){
+    	ArrayList<String> parameters = new ArrayList<String>();
+    	for(String word : parameterString.split(" ")) {
+    	    parameters.add(word);
+    	}
+    	SearchParameter searchParam = new SearchParameter(parameters);
+    	return searchParam;
+    }
+    
+ // This method process filter parameter into a FilterParameter instance
+    // @param parameterString
+    //			string contains parameter data
+    // @return filterParam
+    //			FilterParameter instance contains parameter for filter method
+    
+    public FilterParameter processFilterParameter(String parameterString){
+    	ArrayList<String> parameters = new ArrayList<String>();
+    	for(String word : parameterString.split(" ")) {
+    	    parameters.add(word);
+    	}
+    	FilterParameter filterParam = new FilterParameter(parameters);
+    	return filterParam;
+    }
+    
+    // This method process modify parameter into a DataParameter instance
+    // @param parameterString
+    //			string contains parameter data
+    // @return modifyParam
+    //			DataParameter instance contains parameter for modify method
+    
+    public DataParameterStub processModifyParameter(String parameterString){
+    	DataParameterStub modifyParam = new DataParameterStub();
+    	ArrayList<String> parameters = new ArrayList<String>();
+    	for(String word : parameterString.split(" ")) {
+    	    parameters.add(word);
+    	}
+    	TaskType original = processTaskTypeFromString(parameters.get(_firstArrayIndex));
+    	modifyParam.setOriginalTaskType(original);
+    	modifyParam.setTaskID(Integer.parseInt(parameters.get(_firstArrayIndex+1)));
+    	if (parameters.contains("-totype")){
+    		int indexOfNewTaskType = parameters.indexOf("-totype")+1;
+    		TaskType newType = processTaskTypeFromString(parameters.get(indexOfNewTaskType));
+    		modifyParam.setNewTaskType(newType);
+    	} else {
+    		modifyParam.setNewTaskType(original);
+    	}
+    	if (parameters.contains("-priority")){
+    		int indexOfPriority = parameters.indexOf("-priority")+1;
+    		char priority = parameters.get(indexOfPriority).toCharArray()[_firstArrayIndex];
+    		modifyParam.setPriority(priority);
+    	}
+    	if (parameters.contains("-start")){
+    		int indexOfStartDate = parameters.indexOf("-start")+1;
+    		try{
+    			Date startDate = DateUtil.parse(parameters.get(indexOfStartDate));
+    			modifyParam.setStartDate(startDate);
+    		} catch (Exception e){
+    			
+    		}
+    	}
+    	if (parameters.contains("-end")){
+    		int indexOfEndDate = parameters.indexOf("-end")+1;
+    		try{
+    			Date endDate = DateUtil.parse(parameters.get(indexOfEndDate));
+    			modifyParam.setEndDate(endDate);
+    		} catch (Exception e){
+    			
+    		}
+    	}
+    	if (parameters.contains("-description")){
+    		String newDescription;
+    		int indexOfDescription = parameterString.indexOf("-description");
+    		newDescription = parameterString.substring(indexOfDescription+13);
+    		int indexOfEndOfDescription = newDescription.indexOf("-");
+    		if (indexOfEndOfDescription!=-1){
+    			newDescription = newDescription.substring(0, indexOfEndOfDescription-1);
+    		}
+    		modifyParam.setDescription(newDescription);
+    	}
+    	return modifyParam;
+    }
+    
+ // This method process delete/complete parameter into a DataParameter instance
+    // @param parameterString
+    //			string contains parameter data
+    // @return markDeleteParam
+    //			DataParameter instance contains parameter for delete/complete method
+    
+    public DataParameterStub processMarkDeleteParameter(String parameterString){
+    	DataParameterStub markDeleteParam = new DataParameterStub();
+    	ArrayList<String> parameters = new ArrayList<String>();
+    	for(String word : parameterString.split(" ")) {
+    	    parameters.add(word);
+    	}
+    	TaskType original = processTaskTypeFromString(parameters.get(_firstArrayIndex));
+    	markDeleteParam.setOriginalTaskType(original);
+    	markDeleteParam.setTaskID(Integer.parseInt(parameters.get(_firstArrayIndex+1)));
+    	return markDeleteParam;
+    }
+    
+    // This method process TaskType from String
+    // @param taskTypeString
+    //			string contains task type
+    // @return taskType
+    //			TaskType indicated by taskTypeString
+    
+    public TaskType processTaskTypeFromString(String taskTypeString){
+    	if (taskTypeString.equals("todo")){
+    		return TaskType.TODO;
+    	} else if (taskTypeString.equals("deadline")){
+    		return TaskType.DEADLINE;
+    	} else if (taskTypeString.equals("event")){
+    		return TaskType.EVENT;
+    	} else {
+    		return TaskType.UNKOWN;
+    	}
+    }
+}
---
+++ src/main/java/sg/edu/nus/cs2103t/mina/dao/impl/FileTaskSetDaoImpl.java
@@ -0,0 +1,148 @@
+package sg.edu.nus.cs2103t.mina.dao.impl;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import sg.edu.nus.cs2103t.mina.dao.TaskSetDao;
+import sg.edu.nus.cs2103t.mina.model.Task;
+import sg.edu.nus.cs2103t.mina.model.TaskType;
+
+/**
+ * 
+ * File based implementation for TaskSetDao
+ * 
+ * @author wgx731
+ * @author viettrung9012
+ * @author duzhiyuan
+ * @author joannemah
+ */
+public class FileTaskSetDaoImpl implements TaskSetDao {
+
+    private static Logger logger = LogManager
+            .getLogger(FileTaskSetDaoImpl.class.getName());
+
+    private static final String INVALID_TASK_TYPE = "The given task type is invalid.";
+    private static final String INVALID_FILE_STORAGE = "The given file path is an invalid file storage.";
+    private static final String COMPLETED_SUFFIX = ".compl";
+
+    public static String getCompletedSuffix() {
+        return COMPLETED_SUFFIX;
+    }
+
+    private Map<TaskType, String> _fileLocationMap;
+
+    /**
+     * Create a new file based task set storage with all required file names
+     * 
+     * @param fileLocationMap map of all the supported taskType with file path
+     */
+    public FileTaskSetDaoImpl(Map<TaskType, String> fileLocationMap) {
+        super();
+        _fileLocationMap = fileLocationMap;
+    }
+
+    private String getFileLocation(TaskType taskType, boolean isCompleted) {
+        String fileLocation = _fileLocationMap.get(taskType);
+        if (isCompleted) {
+            fileLocation += COMPLETED_SUFFIX;
+        }
+        return fileLocation;
+    }
+
+    private void createFileIfNotExist(String fileName) throws IOException {
+        File f = new File(fileName);
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+    }
+
+    private ObjectOutput getOutputWriter(String fileName) {
+        OutputStream file;
+        try {
+            file = new FileOutputStream(fileName);
+            OutputStream buffer = new BufferedOutputStream(file);
+            return new ObjectOutputStream(buffer);
+        } catch (FileNotFoundException e) {
+            logger.error(e, e);
+            return null;
+        } catch (IOException e) {
+            logger.error(e, e);
+            return null;
+        }
+    }
+
+    private ObjectInput getInputReader(String fileName) {
+        InputStream file;
+        try {
+            file = new FileInputStream(fileName);
+            InputStream buffer = new BufferedInputStream(file);
+            return new ObjectInputStream(buffer);
+        } catch (FileNotFoundException e) {
+            logger.error(e, e);
+            return null;
+        } catch (IOException e) {
+            logger.error(e, e);
+            return null;
+        }
+    }
+
+    @Override
+    public void saveTaskSet(Set<? extends Task<?>> taskSet, TaskType taskType,
+            boolean isCompleted) throws IOException, IllegalArgumentException {
+        if (!_fileLocationMap.containsKey(taskType)) {
+            throw new IllegalArgumentException(INVALID_TASK_TYPE);
+        }
+        String fileLocation = getFileLocation(taskType, isCompleted);
+        createFileIfNotExist(fileLocation);
+        ObjectOutput output = getOutputWriter(fileLocation);
+        output.writeObject(taskSet);
+        output.close();
+    }
+
+    @Override
+    public Set<? extends Task<?>> loadTaskSet(TaskType taskType,
+            boolean isCompleted) throws IOException, IllegalArgumentException {
+        if (!_fileLocationMap.containsKey(taskType)) {
+            throw new IllegalArgumentException(INVALID_TASK_TYPE);
+        }
+        String fileLocation = getFileLocation(taskType, isCompleted);
+        File f = new File(fileLocation);
+        if (!f.exists() || !f.isFile()) {
+            throw new IOException(INVALID_FILE_STORAGE);
+        }
+        ObjectInput input = getInputReader(fileLocation);
+        try {
+            Object object = input.readObject();
+            if (object == null) {
+                throw new IOException(INVALID_FILE_STORAGE);
+            }
+            @SuppressWarnings("unchecked")
+            Set<? extends Task<?>> content = (Set<? extends Task<?>>) object;
+            input.close();
+            return content;
+        } catch (ClassNotFoundException e) {
+            logger.error(e, e);
+            return null;
+        } catch (NullPointerException e) {
+            logger.error(e, e);
+            return null;
+        }
+    }
+
+}
