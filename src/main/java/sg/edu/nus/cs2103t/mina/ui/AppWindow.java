e1ede6e8479dd88253727500197ef448e7ad1217
viettrung9012 <viettrung9012@localhost>
2014-03-10 16:34:22 +0800

    <feat>(CC, UI): add interaction between CC and UI
    1. add display tasks for UI
    2. add exit function for UI
    3. add process output for CC

--- src/main/java/sg/edu/nus/cs2103t/mina/ui/AppWindow.java
+++ src/main/java/sg/edu/nus/cs2103t/mina/ui/AppWindow.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.cs2103t.mina.ui;
 
+import java.util.ArrayList;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
@@ -14,15 +16,24 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.custom.CBanner;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.List;
+
+import sg.edu.nus.cs2103t.mina.controller.CommandController;
+import sg.edu.nus.cs2103t.mina.controller.DataSyncManager;
+import sg.edu.nus.cs2103t.mina.controller.TaskDataManager;
+import sg.edu.nus.cs2103t.mina.controller.TaskFilterManager;
 
 public class AppWindow {
 
 	protected Shell shell;
 	private Display display;
 	private Text userInputTextField;
+	
+	private static CommandController _cc;
 
 	/**
 	 * Launch the application.
@@ -30,6 +41,7 @@ public class AppWindow {
 	 */
 	public static void main(String[] args) {
 		try {
+			_cc = new CommandController();
 			AppWindow window = new AppWindow();
 			window.open();
 		} catch (Exception e) {
@@ -64,13 +76,13 @@ public class AppWindow {
 	protected void createContents() {
 		shell = new Shell(display, SWT.NO_TRIM | SWT.ON_TOP);
 		shell.setBackground(SWTResourceManager.getColor(0, 0, 0));
-		shell.setSize(1080, 720);
+		shell.setSize(1080, 540);
 		shell.setText("SWT Application");
 		
 		userInputTextField = new Text(shell, SWT.NONE);
 		userInputTextField.setForeground(SWTResourceManager.getColor(0, 51, 0));
 		userInputTextField.setFont(SWTResourceManager.getFont("Comic Sans MS", 15, SWT.NORMAL));
-		userInputTextField.setBounds(4, 680, 1076, 36);
+		userInputTextField.setBounds(4, 500, 1072, 36);
 		
 		Label lblEvent = new Label(shell, SWT.NONE);
 		lblEvent.setAlignment(SWT.CENTER);
@@ -96,24 +108,64 @@ public class AppWindow {
 		lblTodo.setBounds(722, 4, 354, 36);
 		lblTodo.setText("To-do(td)");
 		
-		List eventList = new List(shell, SWT.NONE);
+		final List eventList = new List(shell, SWT.NONE);
 		eventList.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		eventList.setFont(SWTResourceManager.getFont("Comic Sans MS", 15, SWT.NORMAL));
 		eventList.setBackground(SWTResourceManager.getColor(SWT.COLOR_DARK_CYAN));
-		eventList.setBounds(4, 40, 354, 636);
+		eventList.setBounds(4, 40, 354, 456);
 		
-		List deadlineList = new List(shell, SWT.NONE);
+		final List deadlineList = new List(shell, SWT.NONE);
+		deadlineList.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		deadlineList.setFont(SWTResourceManager.getFont("Comic Sans MS", 15, SWT.NORMAL));
 		deadlineList.setBackground(SWTResourceManager.getColor(SWT.COLOR_DARK_CYAN));
-		deadlineList.setEnabled(false);
-		deadlineList.setBounds(362, 40, 356, 636);
+		deadlineList.setBounds(362, 40, 356, 456);
 		
-		List todoList = new List(shell, SWT.NONE);
+		final List todoList = new List(shell, SWT.NONE);
+		todoList.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		todoList.setFont(SWTResourceManager.getFont("Comic Sans MS", 15, SWT.NORMAL));
 		todoList.setBackground(SWTResourceManager.getColor(SWT.COLOR_DARK_CYAN));
-		todoList.setEnabled(false);
-		todoList.setBounds(722, 40, 354, 636);
-		
-		for (int loopIndex = 0; loopIndex < 10; loopIndex++) {
-		      eventList.add("Event " + loopIndex);
-		}
+		todoList.setBounds(722, 40, 354, 456);
+
+		ArrayList<String> todo = _cc.getTodoTask();
+   	 	for (int i=0; i<todo.size(); i++){
+   	 		todoList.add(todo.get(i));
+   	 	}
+   	 	ArrayList<String> deadline = _cc.getDeadlineTask();
+   	 	for (int i=0; i<deadline.size(); i++){
+   	 		deadlineList.add(deadline.get(i));
+   	 	}   	 	
+   	 	ArrayList<String> event = _cc.getEventTask();
+   	 	for (int i=0; i<event.size(); i++){
+   	 		eventList.add(event.get(i));
+   	 	}
+   	 
+		userInputTextField.addKeyListener(new KeyAdapter(){
+	         public void keyPressed(KeyEvent e) {
+	             int key = e.keyCode;
+	             if (key == SWT.CR) {
+	            	 _cc.processUserInput(userInputTextField.getText());
+	            	 if (userInputTextField.getText().equals("exit")){
+	            		 shell.close();
+	            	 } else {
+	            	 userInputTextField.setText("");
+	            	 todoList.removeAll();
+	            	 ArrayList<String> todo = _cc.getTodoTask();
+	            	 for (int i=0; i<todo.size(); i++){
+	            		 todoList.add(todo.get(i));
+	            	 }
+	            	 deadlineList.removeAll();
+	            	 ArrayList<String> deadline = _cc.getDeadlineTask();
+	            	 for (int i=0; i<deadline.size(); i++){
+	            		 deadlineList.add(deadline.get(i));
+	            	 }
+	            	 eventList.removeAll();
+	            	 ArrayList<String> event = _cc.getEventTask();
+	            	 for (int i=0; i<event.size(); i++){
+	            		 eventList.add(event.get(i));
+	            	 }
+	            	}
+	                }
+	             }
+	           });
 	}
 }
